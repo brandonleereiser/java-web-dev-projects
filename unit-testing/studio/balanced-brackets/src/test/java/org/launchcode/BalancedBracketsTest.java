f6faf06f35862bef5790c5be7e5c8a43519edeaa
Brandon Reiser <reiserb58@gmail.com>
2023-12-18 17:40:01 -0600

    update for exercises and studios

--- unit-testing/studio/balanced-brackets/src/test/java/org/launchcode/BalancedBracketsTest.java
+++ unit-testing/studio/balanced-brackets/src/test/java/org/launchcode/BalancedBracketsTest.java
@@ -10,4 +10,62 @@ class BalancedBracketsTest {
     public void emptyTest() {
         assertEquals(true, true);
     }
+
+    @Test
+    public void onlyBracketsReturnsTrue(){
+        assertTrue(BalancedBrackets.hasBalancedBrackets("[]"));
+    }
+
+    @Test
+    public void testOrderOfBrackets(){
+        assertFalse(BalancedBrackets.hasBalancedBrackets("]["));
+    }
+
+    @Test
+    public void testHasPairOfBracketsEnd(){
+        assertFalse(BalancedBrackets.hasBalancedBrackets("["));
+    }
+    @Test
+    public void testHasPairOfBracketsStart(){
+        assertFalse(BalancedBrackets.hasBalancedBrackets("]"));
+    }
+    @Test
+    public void testNothingIsTrue(){
+        assertTrue(BalancedBrackets.hasBalancedBrackets(""));
+    }
+
+    @Test
+    public void testMixedUpBracketsAfterCorrectOnes(){
+        assertFalse(BalancedBrackets.hasBalancedBrackets("[]]["));
+    }
+
+    @Test
+    public void testNoBracketsWithWordsIsTrue(){
+        assertTrue(BalancedBrackets.hasBalancedBrackets("It's Halloween!"));
+    }
+
+    @Test
+    public void testBracketsWithWordsIsTrue(){
+        assertTrue(BalancedBrackets.hasBalancedBrackets("[It's Halloween!]"));
+    }
+    @Test
+    public void testBracketsWithWordsOOOIsFalse(){
+        assertFalse(BalancedBrackets.hasBalancedBrackets("]It's Halloween!["));
+    }
+    @Test
+    public void testBracketsInMiddleOfWordsFalse(){
+        assertFalse(BalancedBrackets.hasBalancedBrackets("It's ]Halloween[!"));
+    }
+
+    @Test
+    public void testBracketsInMiddleOfWordsTrue(){
+        assertTrue(BalancedBrackets.hasBalancedBrackets("It's [Halloween]!"));
+    }
+
+    @Test
+    public void UnevenBracketsWithWordsIsFalse(){
+        assertFalse(BalancedBrackets.hasBalancedBrackets("It'[s [Halloween]!"));
+    }
+
+
 }
